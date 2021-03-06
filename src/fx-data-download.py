47b24dbad8e933883da07129503aa3acb1cf46d6
Madduri Mahesh Vardhan <58556578+maddurimaheshvardhan@users.noreply.github.com>
2021-03-06 16:13:01 +0530

    Code Fixes

--- src/fx-data-download.py
+++ src/fx-data-download.py
@@ -319,8 +319,8 @@ all_currencies = {
 
 
 class Dukascopy:
-    url_tpl = ("'http://www.dukascopy.com/'\
-               'datafeed/%s/%04d/%02d/%02d/%02dh_ticks.bi5'")
+    url_tpl = ("http://www.dukascopy.com/"
+               "datafeed/%s/%04d/%02d/%02d/%02dh_ticks.bi5")
 
     def __init__(self, pair, year, month, day, hour,
                  dest="download/dukascopy"):
